5c2de5abb7076f21ca587ce87192d9ecd4954b0d
Ifeoluwa Olubo <ifeoluwaolubo@gmail.com>
2022-05-03 14:12:55 +0100

    Finished the registeration UI

---
+++ chatty/src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "http://localhost:4000";
+export const loginRoute = `${host}/api/v1/auth/login`;
+export const registerRoute = `${host}/api/v1/auth/register`;
+export const logoutRoute = `${host}/api/v1/auth/logout`;
+export const allUsersRoute = `${host}/api/v1/auth/users`;
+export const sendMessageRoute = `${host}/api/v1/messages/send`;
+export const receiveMessageRoute = `${host}/api/v1/messages/receive`;
+export const setAvatarRoute = `${host}/api/v1/auth/set-avatar`;
